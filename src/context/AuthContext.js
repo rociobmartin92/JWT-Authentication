3f8edfbb00ef4b7936e8d36979094f717e6a4329
rocio_martin98 <rocio_martin98@hotmail.com>
2023-03-13 18:15:59 -0300

    sdfs

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,3 +1,4 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import React, { createContext, useState } from "react";
 
 export const UserContext = createContext();
@@ -6,8 +7,62 @@ const AuthContext = ({ children }) => {
   const [isLoading, setIsLoading] = useState(false);
   const [userToken, setUserToken] = useState(null);
 
+  // SET ITEM
+  const login = async (navigation) => {
+    setIsLoading(true);
+
+    try {
+      await AsyncStorage.setItem("userToken", value);
+    } catch (e) {
+      // Poner una alerta con el error al usuario
+      throw new Error(e);
+    }
+    setIsLoading(false);
+    navigation.navigate("LoginScreen");
+  };
+
+  // REMOVE ITEM
+  const logout = async () => {
+    setIsLoading(true);
+    setUserToken(null);
+    try {
+      await AsyncStorage.removeItem("userToken");
+    } catch (e) {
+      // Poner una alerta con el error al usuario
+      throw new Error(e);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const signUp = (navigation) => {
+    navigation.navigate("RegisterScreen");
+  };
+
+  // GET ITEM // Funcion para que la App quede abierta luego de una autenticacion exitosa
+  const keepLoged = async () => {
+    setIsLoading(true);
+    try {
+      const logedInToken = await AsyncStorage.getItem("userToken");
+      setUserToken(logedInToken);
+    } catch (e) {
+      // Poner una alerta con el error al usuario
+      throw new Error(e);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    keepLoged();
+  }, []);
+
   return (
-    <UserContext.Provider value={{ isLoading, userToken }}>{children}</UserContext.Provider>
+    <UserContext.Provider
+      value={{ isLoading, userToken, login, logout, signUp }}
+    >
+      {children}
+    </UserContext.Provider>
   );
 };
 
--- src/screens/StartScreen.js
+++ src/screens/StartScreen.js
@@ -6,22 +6,16 @@ import Button from "../components/Button";
 import { UserContext } from "../context/AuthContext";
 
 export default function StartScreen({ navigation }) {
-
+  const { login, signUp } = useContext(UserContext);
 
   return (
     <Background>
       <Logo />
       <Header> Auth with JWT and OAuth</Header>
-      <Button
-        mode="contained"
-        onPress={() => navigation.navigate("LoginScreen")}
-      >
+      <Button mode="contained" onPress={() => login(navigation)}>
         Login
       </Button>
-      <Button
-        mode="outlined"
-        onPress={() => navigation.navigate("RegisterScreen")}
-      >
+      <Button mode="outlined" onPress={() => signUp(navigation)}>
         Sign Up
       </Button>
     </Background>
